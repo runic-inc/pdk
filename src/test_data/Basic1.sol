f0e3d016807f4870e72567c75c288013283830a3
Rob Green <rbgrn@rbgrn.net>
2024-05-12 22:04:26 -0700

    Support address type and fix field ordering to work with 160 bit fields

--- src/test_data/Basic1.sol
+++ src/test_data/Basic1.sol
@@ -8,11 +8,12 @@ import "@patchwork/PatchworkUtils.sol";
 contract Basic1ComplexName is Patchwork721 {
 
     struct Metadata {
+        address addr;
+        string c8;
+        uint32 fieldu32;
         string name;
         uint128 fieldu128a;
         uint128 fieldu128b;
-        string c8;
-        uint32 fieldu32;
         uint16 fieldu16;
     }
 
@@ -40,86 +41,58 @@ contract Basic1ComplexName is Patchwork721 {
     }
 
     function schema() pure external override returns (MetadataSchema memory) {
-        MetadataSchemaEntry[] memory entries = new MetadataSchemaEntry[](6);
-        entries[0] = MetadataSchemaEntry(1, 0, FieldType.CHAR16, 1, FieldVisibility.PUBLIC, 0, 0, "name");
-        entries[1] = MetadataSchemaEntry(3, 0, FieldType.UINT128, 1, FieldVisibility.PUBLIC, 0, 128, "fieldu128a");
-        entries[2] = MetadataSchemaEntry(4, 0, FieldType.UINT128, 1, FieldVisibility.PUBLIC, 1, 0, "fieldu128b");
-        entries[3] = MetadataSchemaEntry(6, 0, FieldType.CHAR8, 1, FieldVisibility.PUBLIC, 1, 128, "c8");
-        entries[4] = MetadataSchemaEntry(5, 0, FieldType.UINT32, 1, FieldVisibility.PUBLIC, 1, 192, "fieldu32");
-        entries[5] = MetadataSchemaEntry(2, 0, FieldType.UINT16, 1, FieldVisibility.PUBLIC, 1, 224, "fieldu16");
+        MetadataSchemaEntry[] memory entries = new MetadataSchemaEntry[](7);
+        entries[0] = MetadataSchemaEntry(7, 0, FieldType.ADDRESS, 1, FieldVisibility.PUBLIC, 0, 0, "addr");
+        entries[1] = MetadataSchemaEntry(6, 0, FieldType.CHAR8, 1, FieldVisibility.PUBLIC, 0, 160, "c8");
+        entries[2] = MetadataSchemaEntry(5, 0, FieldType.UINT32, 1, FieldVisibility.PUBLIC, 0, 224, "fieldu32");
+        entries[3] = MetadataSchemaEntry(1, 0, FieldType.CHAR16, 1, FieldVisibility.PUBLIC, 1, 0, "name");
+        entries[4] = MetadataSchemaEntry(3, 0, FieldType.UINT128, 1, FieldVisibility.PUBLIC, 1, 128, "fieldu128a");
+        entries[5] = MetadataSchemaEntry(4, 0, FieldType.UINT128, 1, FieldVisibility.PUBLIC, 2, 0, "fieldu128b");
+        entries[6] = MetadataSchemaEntry(2, 0, FieldType.UINT16, 1, FieldVisibility.PUBLIC, 2, 128, "fieldu16");
         return MetadataSchema(1, entries);
     }
 
     function packMetadata(Metadata memory data) public pure returns (uint256[] memory slots) {
-        slots = new uint256[](2);
-        slots[0] = PatchworkUtils.strToUint256(data.name) >> 128 | uint256(data.fieldu128a) << 128;
-        slots[1] = uint256(data.fieldu128b) | PatchworkUtils.strToUint256(data.c8) >> 192 << 128 | uint256(data.fieldu32) << 192 | uint256(data.fieldu16) << 224;
+        slots = new uint256[](3);
+        slots[0] = uint256(data.addr) | PatchworkUtils.strToUint256(data.c8) >> 192 << 160 | uint256(data.fieldu32) << 224;
+        slots[1] = PatchworkUtils.strToUint256(data.name) >> 128 | uint256(data.fieldu128a) << 128;
+        slots[2] = uint256(data.fieldu128b) | uint256(data.fieldu16) << 128;
         return slots;
     }
 
     function unpackMetadata(uint256[] memory slots) public pure returns (Metadata memory data) {
         uint256 slot = slots[0];
+        data.addr = address(slot);
+        data.c8 = PatchworkUtils.toString8(uint64(slot >> 160));
+        data.fieldu32 = uint32(slot >> 224);
+        slot = slots[1];
         data.name = PatchworkUtils.toString16(uint128(slot));
         data.fieldu128a = uint128(slot >> 128);
-        slot = slots[1];
+        slot = slots[2];
         data.fieldu128b = uint128(slot);
-        data.c8 = PatchworkUtils.toString8(uint64(slot >> 128));
-        data.fieldu32 = uint32(slot >> 192);
-        data.fieldu16 = uint16(slot >> 224);
+        data.fieldu16 = uint16(slot >> 128);
         return data;
     }
 
-    // Load Only name
-    function loadName(uint256 tokenId) public view returns (string memory) {
+    // Load Only addr
+    function loadAddr(uint256 tokenId) public view returns (address) {
         uint256 value = uint256(_metadataStorage[tokenId][0]);
-        return PatchworkUtils.toString16(uint128(value));
+        return address(value);
     }
 
-    // Store Only name
-    function storeName(uint256 tokenId, string memory name) public {
+    // Store Only addr
+    function storeAddr(uint256 tokenId, address addr) public {
         if (!_checkTokenWriteAuth(tokenId)) {
             revert IPatchworkProtocol.NotAuthorized(msg.sender);
         }
-        uint256 mask = (1 << 128) - 1;
+        uint256 mask = (1 << 160) - 1;
         uint256 cleared = uint256(_metadataStorage[tokenId][0]) & ~(mask);
-        _metadataStorage[tokenId][0] = cleared | (PatchworkUtils.strToUint256(name) >> 128 & mask);
-    }
-
-    // Load Only fieldu128a
-    function loadFieldu128a(uint256 tokenId) public view returns (uint128) {
-        uint256 value = uint256(_metadataStorage[tokenId][0]) >> 128;
-        return uint128(value);
-    }
-
-    // Store Only fieldu128a
-    function storeFieldu128a(uint256 tokenId, uint128 fieldu128a) public {
-        if (!_checkTokenWriteAuth(tokenId)) {
-            revert IPatchworkProtocol.NotAuthorized(msg.sender);
-        }
-        uint256 mask = (1 << 128) - 1;
-        uint256 cleared = uint256(_metadataStorage[tokenId][0]) & ~(mask << 128);
-        _metadataStorage[tokenId][0] = cleared | (uint256(fieldu128a) & mask) << 128;
-    }
-
-    // Load Only fieldu128b
-    function loadFieldu128b(uint256 tokenId) public view returns (uint128) {
-        uint256 value = uint256(_metadataStorage[tokenId][1]);
-        return uint128(value);
-    }
-
-    // Store Only fieldu128b
-    function storeFieldu128b(uint256 tokenId, uint128 fieldu128b) public {
-        if (!_checkTokenWriteAuth(tokenId)) {
-            revert IPatchworkProtocol.NotAuthorized(msg.sender);
-        }
-        uint256 mask = (1 << 128) - 1;
-        uint256 cleared = uint256(_metadataStorage[tokenId][1]) & ~(mask);
-        _metadataStorage[tokenId][1] = cleared | (uint256(fieldu128b) & mask);
+        _metadataStorage[tokenId][0] = cleared | (uint256(addr) & mask);
     }
 
     // Load Only c8
     function loadC8(uint256 tokenId) public view returns (string memory) {
-        uint256 value = uint256(_metadataStorage[tokenId][1]) >> 128;
+        uint256 value = uint256(_metadataStorage[tokenId][0]) >> 160;
         return PatchworkUtils.toString8(uint64(value));
     }
 
@@ -129,13 +102,13 @@ contract Basic1ComplexName is Patchwork721 {
             revert IPatchworkProtocol.NotAuthorized(msg.sender);
         }
         uint256 mask = (1 << 64) - 1;
-        uint256 cleared = uint256(_metadataStorage[tokenId][1]) & ~(mask << 128);
-        _metadataStorage[tokenId][1] = cleared | (PatchworkUtils.strToUint256(c8) >> 192 & mask) << 128;
+        uint256 cleared = uint256(_metadataStorage[tokenId][0]) & ~(mask << 160);
+        _metadataStorage[tokenId][0] = cleared | (PatchworkUtils.strToUint256(c8) >> 192 & mask) << 160;
     }
 
     // Load Only fieldu32
     function loadFieldu32(uint256 tokenId) public view returns (uint32) {
-        uint256 value = uint256(_metadataStorage[tokenId][1]) >> 192;
+        uint256 value = uint256(_metadataStorage[tokenId][0]) >> 224;
         return uint32(value);
     }
 
@@ -145,13 +118,61 @@ contract Basic1ComplexName is Patchwork721 {
             revert IPatchworkProtocol.NotAuthorized(msg.sender);
         }
         uint256 mask = (1 << 32) - 1;
-        uint256 cleared = uint256(_metadataStorage[tokenId][1]) & ~(mask << 192);
-        _metadataStorage[tokenId][1] = cleared | (uint256(fieldu32) & mask) << 192;
+        uint256 cleared = uint256(_metadataStorage[tokenId][0]) & ~(mask << 224);
+        _metadataStorage[tokenId][0] = cleared | (uint256(fieldu32) & mask) << 224;
+    }
+
+    // Load Only name
+    function loadName(uint256 tokenId) public view returns (string memory) {
+        uint256 value = uint256(_metadataStorage[tokenId][1]);
+        return PatchworkUtils.toString16(uint128(value));
+    }
+
+    // Store Only name
+    function storeName(uint256 tokenId, string memory name) public {
+        if (!_checkTokenWriteAuth(tokenId)) {
+            revert IPatchworkProtocol.NotAuthorized(msg.sender);
+        }
+        uint256 mask = (1 << 128) - 1;
+        uint256 cleared = uint256(_metadataStorage[tokenId][1]) & ~(mask);
+        _metadataStorage[tokenId][1] = cleared | (PatchworkUtils.strToUint256(name) >> 128 & mask);
+    }
+
+    // Load Only fieldu128a
+    function loadFieldu128a(uint256 tokenId) public view returns (uint128) {
+        uint256 value = uint256(_metadataStorage[tokenId][1]) >> 128;
+        return uint128(value);
+    }
+
+    // Store Only fieldu128a
+    function storeFieldu128a(uint256 tokenId, uint128 fieldu128a) public {
+        if (!_checkTokenWriteAuth(tokenId)) {
+            revert IPatchworkProtocol.NotAuthorized(msg.sender);
+        }
+        uint256 mask = (1 << 128) - 1;
+        uint256 cleared = uint256(_metadataStorage[tokenId][1]) & ~(mask << 128);
+        _metadataStorage[tokenId][1] = cleared | (uint256(fieldu128a) & mask) << 128;
+    }
+
+    // Load Only fieldu128b
+    function loadFieldu128b(uint256 tokenId) public view returns (uint128) {
+        uint256 value = uint256(_metadataStorage[tokenId][2]);
+        return uint128(value);
+    }
+
+    // Store Only fieldu128b
+    function storeFieldu128b(uint256 tokenId, uint128 fieldu128b) public {
+        if (!_checkTokenWriteAuth(tokenId)) {
+            revert IPatchworkProtocol.NotAuthorized(msg.sender);
+        }
+        uint256 mask = (1 << 128) - 1;
+        uint256 cleared = uint256(_metadataStorage[tokenId][2]) & ~(mask);
+        _metadataStorage[tokenId][2] = cleared | (uint256(fieldu128b) & mask);
     }
 
     // Load Only fieldu16
     function loadFieldu16(uint256 tokenId) public view returns (uint16) {
-        uint256 value = uint256(_metadataStorage[tokenId][1]) >> 224;
+        uint256 value = uint256(_metadataStorage[tokenId][2]) >> 128;
         return uint16(value);
     }
 
@@ -161,7 +182,7 @@ contract Basic1ComplexName is Patchwork721 {
             revert IPatchworkProtocol.NotAuthorized(msg.sender);
         }
         uint256 mask = (1 << 16) - 1;
-        uint256 cleared = uint256(_metadataStorage[tokenId][1]) & ~(mask << 224);
-        _metadataStorage[tokenId][1] = cleared | (uint256(fieldu16) & mask) << 224;
+        uint256 cleared = uint256(_metadataStorage[tokenId][2]) & ~(mask << 128);
+        _metadataStorage[tokenId][2] = cleared | (uint256(fieldu16) & mask) << 128;
     }
 }
