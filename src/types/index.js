7692757b007476e7db2d126d193eaa0698bccb8d
Rob Green <rbgrn@rbgrn.net>
2024-08-16 14:59:04 -0700

    working in tests

---
+++ src/types/index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Feature = void 0;
+var Feature;
+(function (Feature) {
+    Feature["FRAGMENTMULTI"] = "FRAGMENTMULTI";
+    Feature["FRAGMENTSINGLE"] = "FRAGMENTSINGLE";
+    Feature["PATCH"] = "PATCH";
+    Feature["PATCHACCOUNT"] = "ACCOUNTPATCH";
+    Feature["PATCH1155"] = "1155PATCH";
+    Feature["MINTABLE"] = "MINTABLE";
+    Feature["REVERSIBLE"] = "REVERSIBLE";
+    Feature["WEAKREF"] = "WEAKREF";
+    Feature["DYNAMICREFLIBRARY"] = "DYNAMICREFLIBRARY";
+})(Feature = exports.Feature || (exports.Feature = {}));
