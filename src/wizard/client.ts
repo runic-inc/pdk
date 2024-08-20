1e0562db15c949cf26e8343c35faf4a58d08004f
Brock Petrie <brockpetrie@gmail.com>
2024-08-20 17:32:04 -0500

    working

--- src/wizard/client.ts
+++
@@ -1,5 +0,0 @@
-import './styles/tailwind.css';
-
-// This file is the entry point for Vite
-// It imports the CSS so that Vite can process it
-// You can add any client-side JavaScript here if needed
