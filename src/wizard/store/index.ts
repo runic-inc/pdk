bfbef0e0c571c1d06897c3cf11319bc935494d59
brockpetrie <brockpetrie@gmail.com>
2024-08-26 22:37:54 -0500

    Wizard store persistence

--- src/wizard/store/index.ts
+++ src/wizard/store/index.ts
@@ -4,6 +4,7 @@ import defaultContract from '@/wizard/lib/defaultContract';
 import defaultScope from '@/wizard/lib/defaultScope';
 import { nanoid } from 'nanoid';
 import { UContractConfig } from '../types';
+import { persist } from 'zustand/middleware';
 
 type EditorState = {
     scopeConfig: ScopeConfig;
@@ -19,56 +20,63 @@ type EditorState = {
     updateContractsConfig: (newConfigs: UContractConfig[]) => void;
 };
 
-const useStore = create<EditorState>()((set, get) => ({
-    scopeConfig: defaultScope,
-    updateScopeConfig: (config: ScopeConfig) => set({ scopeConfig: config }),
-    contractsConfig: [defaultContract],
-    editor: defaultContract._uid,
-    contractConfig: defaultContract,
-    getContractConfig: () => {
-        return {
-            ...get().contractsConfig.find((config) => config._uid === get().editor)!,
-            scopeName: get().scopeConfig.name,
-        };
-    },
-    setEditor: (id: string | null) => set({ editor: id }),
-    addNewContract: () => {
-        const id = nanoid();
-        set({
-            contractsConfig: [
-                ...get().contractsConfig,
-                {
-                    ...defaultContract,
-                    _uid: id,
-                    name: 'New Contract ' + (get().contractsConfig.length + 1),
-                },
-            ],
-        });
-        return id;
-    },
-    updateContractConfig: (newConfig: UContractConfig) => {
-        set({
-            contractsConfig: get().contractsConfig.map((config) => {
-                if (config._uid === get().editor) {
-                    return newConfig;
+const useStore = create<EditorState>()(
+    persist(
+        (set, get) => ({
+            scopeConfig: defaultScope,
+            updateScopeConfig: (config: ScopeConfig) => set({ scopeConfig: config }),
+            contractsConfig: [defaultContract],
+            editor: defaultContract._uid,
+            contractConfig: defaultContract,
+            getContractConfig: () => {
+                return {
+                    ...get().contractsConfig.find((config) => config._uid === get().editor)!,
+                    scopeName: get().scopeConfig.name,
+                };
+            },
+            setEditor: (id: string | null) => set({ editor: id }),
+            addNewContract: () => {
+                const id = nanoid();
+                set({
+                    contractsConfig: [
+                        ...get().contractsConfig,
+                        {
+                            ...defaultContract,
+                            _uid: id,
+                            name: 'New Contract ' + (get().contractsConfig.length + 1),
+                        },
+                    ],
+                });
+                return id;
+            },
+            updateContractConfig: (newConfig: UContractConfig) => {
+                set({
+                    contractsConfig: get().contractsConfig.map((config) => {
+                        if (config._uid === get().editor) {
+                            return newConfig;
+                        }
+                        return config;
+                    }),
+                });
+            },
+            deleteContract: (id: string) => {
+                // get index of current contract
+                if (get().editor === id) {
+                    const index = get().contractsConfig.findIndex((config) => config._uid === id);
+                    const newEditor = get().contractsConfig[index - 1]?._uid || get().contractsConfig[index + 1]?._uid || null;
+                    set({ editor: newEditor });
                 }
-                return config;
-            }),
-        });
-    },
-    deleteContract: (id: string) => {
-        // get index of current contract
-        if (get().editor === id) {
-            const index = get().contractsConfig.findIndex((config) => config._uid === id);
-            const newEditor = get().contractsConfig[index - 1]?._uid || get().contractsConfig[index + 1]?._uid || null;
-            set({ editor: newEditor });
-        }
-        set({
-            contractsConfig: get().contractsConfig.filter((config) => config._uid !== id),
-        });
-    },
-    updateContractsConfig: (newConfigs: UContractConfig[]) => set({ contractsConfig: newConfigs }),
-}));
+                set({
+                    contractsConfig: get().contractsConfig.filter((config) => config._uid !== id),
+                });
+            },
+            updateContractsConfig: (newConfigs: UContractConfig[]) => set({ contractsConfig: newConfigs }),
+        }),
+        {
+            name: 'wizard-store',
+        },
+    ),
+);
 
 export default useStore;
 
