c07afbc1be65fa4397bb91f276297a5a95755248
Kevin Day <kevin.r.day@gmail.comm>
2024-08-28 15:20:32 -0400

    refactors

--- src/configgen/jsonProjectConfigGen.spec.ts
+++ src/configgen/jsonProjectConfigGen.spec.ts
@@ -1,6 +1,6 @@
 
-import { ProjectConfigGen } from "./projectConfigGen";
-import { ProjectConfig, ScopeConfig } from "@/types";
+import { ProjectConfigGen } from "./jsonProjectConfigGen";
+import projectConfig from "../codegen/test_data/project_configs/project-config";
 
 describe("JSONProjectConfigGen", () => {
   it("should generate a project config", async () => {
@@ -40,38 +40,8 @@ describe("JSONProjectConfigGen", () => {
         }
     }
   }`;
-  const projConfig = {
-    name: "My Project",
-    scopes: [
-      {
-        name: "MyScope",
-        owner: "0x222222cf1046e68e36E1aA2E0E07105eDDD1f08E",
-        whitelist: true,
-        userAssign: false,
-        userPatch: false,
-        bankers: ["0x000000254729296a45a3885639AC7E10F9d54979", "Contract1"],
-        operators: ["0x000000111129296a45a3885639AC7E10F9d54979", "Contract1"],
-        mintConfigs: new Map([
-          ["0xc0ffee254729296a45a3885639AC7E10F9d54979", { flatFee: 0, active: true }]
-        ]),
-        patchFees: new Map([
-          ["0x999999cf1046e68e36E1aA2E0E07105eDDD1f08E", 0]
-        ]),
-        assignFees: new Map([
-          ["0x555555cf1046e68e36E1aA2E0E07105eDDD1f08E", 0]
-        ])
-      } as ScopeConfig
-    ],
-    contracts: new Map([
-      ["Contract1", "config1.json"],
-      ["Contract2", "config2.json"]
-    ]),
-    contractRelations: new Map([
-      ["Contract1", { fragments: ["Contract2"] }]
-    ])
-  } as ProjectConfig;
-  const genString = new ProjectConfigGen().gen(projConfig);
-  // console.log(genString);
+  
+  const genString = new ProjectConfigGen().gen(projectConfig);
   expect( JSON.parse(genString)).toEqual(JSON.parse(projectJson));
   });
 });
