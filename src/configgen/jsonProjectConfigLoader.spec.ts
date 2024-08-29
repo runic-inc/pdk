5b74711f9b7aa5ae7f21debad4b4cba57c9f313f
Rob Green <rbgrn@rbgrn.net>
2024-08-28 18:54:54 -0700

    loader

---
+++ src/configgen/jsonProjectConfigLoader.spec.ts
@@ -0,0 +1,47 @@
+
+import { JSONProjectConfigLoader } from "./jsonProjectConfigLoader";
+import projectConfig from "../codegen/test_data/project_configs/project-config";
+
+describe("JSONProjectConfigLoader", () => {
+  it("should load a project config", async () => {
+    const projectJson = `{
+    "name": "My Project",
+    "scopes": {
+        "MyScope": {
+            "owner": "0x222222cf1046e68e36E1aA2E0E07105eDDD1f08E",
+            "whitelist": true,
+            "userAssign": false,
+            "userPatch": false,
+            "bankers": ["0x000000254729296a45a3885639AC7E10F9d54979", "Contract1"],
+            "operators": ["0x000000111129296a45a3885639AC7E10F9d54979", "Contract1"],
+            "mintConfigs": {
+                "0xc0ffee254729296a45a3885639AC7E10F9d54979": {
+                    "flatFee": 0,
+                    "active": true
+                }
+            },
+            "patchFees": {
+                "0x999999cf1046e68e36E1aA2E0E07105eDDD1f08E": 0
+            },
+            "assignFees": {
+                "0x555555cf1046e68e36E1aA2E0E07105eDDD1f08E": 0
+            }
+        }
+    },
+    "contracts": {
+        "Contract1": {
+            "config": "config1.json",
+            "fragments": [
+                "Contract2"
+            ]
+        },
+        "Contract2": {
+            "config": "config2.json"
+        }
+    }
+  }`;
+  
+  const loadedConfig = new JSONProjectConfigLoader().load(projectJson);
+  expect(loadedConfig).toEqual(projectConfig);
+  });
+});
---
+++ src/configgen/jsonProjectConfigLoader.ts
@@ -0,0 +1,41 @@
+import { ContractRelation, MintConfig, ProjectConfig, ScopeConfig } from "@/types";
+
+export class JSONProjectConfigLoader {
+    constructor() {}
+
+    load(jsonString: string): ProjectConfig {
+        let projectConfig = JSON.parse(jsonString);
+        let contractRelations = new Map<string, ContractRelation>();
+        return {
+            name: projectConfig.name,
+            scopes: Object.entries(projectConfig.scopes).map(([key, value]) => {
+                return this.loadScopeConfig(key, value);
+            }),
+            contracts: Object.entries(projectConfig.contracts).reduce((map, [key, value]) => {
+                const v = value as any;
+                map.set(key, v.config);
+                const fragments = v.fragments as string[];
+                if (fragments) {
+                    contractRelations.set(key, { fragments: fragments });
+                }
+                return map;
+            }, new Map<string, string>()),
+            contractRelations: contractRelations
+        };
+    }
+
+    loadScopeConfig(name: string, scopeConfig: any): ScopeConfig {
+        return {
+            name: name,
+            owner: scopeConfig.owner,
+            whitelist: scopeConfig.whitelist,
+            userAssign: scopeConfig.userAssign,
+            userPatch: scopeConfig.userPatch,
+            bankers: scopeConfig.bankers,
+            operators: scopeConfig.operators,
+            mintConfigs: new Map(Object.entries(scopeConfig.mintConfigs)),
+            patchFees: new Map(Object.entries(scopeConfig.patchFees)),
+            assignFees: new Map(Object.entries(scopeConfig.assignFees))
+        };
+    }
+}
