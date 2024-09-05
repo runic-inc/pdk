571e77bf09c4f673faa0e554425ece0f5fe1cf37
brockpetrie <brockpetrie@gmail.com>
2024-09-05 11:49:41 -0500

    Dep removal

--- apps/wizard/tailwind.config.js
+++ apps/wizard/tailwind.config.js
@@ -1,12 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 
-const alphaColor = (color, alpha) => {
-    const hexAlpha = Math.round((alpha / 100) * 255)
-        .toString(16)
-        .padStart(2, '0');
-    return color + hexAlpha;
-};
-
 module.exports = {
   darkMode: ["class"],
   content: [
