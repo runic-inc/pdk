ddf1424366609eea2966f259095f2fefb69e41e4
Kevin Day <kevin.r.day@gmail.comm>
2024-09-06 15:36:12 -0400

    comment

--- packages/common/src/configgen/jsonProjectConfigLoader.spec.ts
+++ packages/common/src/configgen/jsonProjectConfigLoader.spec.ts
@@ -40,7 +40,7 @@ function compareProjectConfigsWithContractConfig(actual: ProjectConfig, expected
 
 describe("JSONProjectConfigLoader", () => {
     it("should load a project config matching project-config.json", async () => {
-        // Keep this test as is
+        // Read the content of the actual project-config.json file
         const projectConfigPath = path.join(__dirname, '../codegen/test_data/project_configs/project-config.json');
         const projectJson = fs.readFileSync(projectConfigPath, 'utf-8');
         const loadedConfig = new JSONProjectConfigLoader().load(projectJson);
