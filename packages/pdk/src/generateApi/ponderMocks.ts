b6c79460088749453da85a9a34c1c2704cfa9a35
Kevin Day <kevin.r.day@gmail.comm>
2024-09-23 16:24:07 -0400

    fix

--- packages/pdk/src/generateApi/ponderMocks.ts
+++ packages/pdk/src/generateApi/ponderMocks.ts
@@ -1,31 +1,30 @@
 interface SchemaBuilder {
-  createTable: (schema: Record<string, FieldType>) => TableDefinition;
-  bigint: () => FieldType;
-  int: () => FieldType;
-  hex: () => FieldType;
-  boolean: () => FieldType;
-}
-
-interface TableDefinition {
-  [key: string]: FieldType;
-}
-
-type FieldType = "bigint" | "int" | "hex" | "boolean" | "string" | string;
-
-export function createSchema(
-  schemaDefinition: (p: SchemaBuilder) => Record<string, TableDefinition>
-) {
-  const mockSchemaBuilder: SchemaBuilder = {
-    createTable: (schema: Record<string, FieldType>) => {
-      return schema;
-    },
-    bigint: () => "bigint",
-    int: () => "int",
-    hex: () => "hex",
-    boolean: () => "boolean",
-    string: () => "string",
-  };
-
-  const result = schemaDefinition(mockSchemaBuilder);
-  return result;
-}
+    createTable: (schema: Record<string, FieldType>) => TableDefinition;
+    bigint: () => FieldType;
+    int: () => FieldType;
+    hex: () => FieldType;
+    boolean: () => FieldType;
+    string: () => FieldType;
+  }
+  
+  interface TableDefinition {
+    [key: string]: FieldType;
+  }
+  
+  type FieldType = 'bigint' | 'int' | 'hex' | 'boolean' | 'string' | string;
+  
+  export function createSchema(schemaDefinition: (p: SchemaBuilder) => Record<string, TableDefinition>) {
+    const mockSchemaBuilder: SchemaBuilder = {
+      createTable: (schema: Record<string, FieldType>) => {
+        return schema;
+      },
+      bigint: () => 'bigint',
+      int: () => 'int',
+      hex: () => 'hex',
+      boolean: () => 'boolean',
+      string: () => 'string',
+    };
+  
+    const result = schemaDefinition(mockSchemaBuilder);
+    return result;
+  }
