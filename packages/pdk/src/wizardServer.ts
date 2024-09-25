6c37220c189d29094cb51b23c0b69da04611f6e5
Rob Green <rbgrn@rbgrn.net>
2024-09-25 15:29:55 -0700

    increment beta to fix wizard static serve

--- packages/pdk/src/wizardServer.ts
+++ packages/pdk/src/wizardServer.ts
@@ -1,15 +1,26 @@
 import { Hono } from 'hono';
 import { serve } from '@hono/node-server';
 import { serveStatic } from '@hono/node-server/serve-static'
+import path from 'path';
 
 export const launchWizardApp = () => {
   const app = new Hono();
 
+  // console.log(process.cwd());
+  // console.log(__dirname);
+  const rel = path.relative(process.cwd(), __dirname);
+  // console.log(rel);
+  
   app.get('/api', async (c) => {
     return c.text('Hello from Patchwork Wizard API');
   });
   
-  app.use('/*', serveStatic({ root: 'dist/wizard' }));
+  app.use('/*', serveStatic({ 
+    root: `${rel}/wizard`, 
+    onNotFound: (path, c) => {
+      // console.log(`${path} is not found, checked ${c.req.path}`)
+    }
+  }));
 
   serve({
     fetch: app.fetch,
