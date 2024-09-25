4ddab15e4628193e8f8b6f2550d91379d9d37317
brockpetrie <brockpetrie@gmail.com>
2024-09-25 13:55:16 -0500

    Workspace reset btn

--- apps/wizard/src/components/Layout.tsx
+++ apps/wizard/src/components/Layout.tsx
@@ -8,6 +8,7 @@ import Toolbar from './Toolbar';
 
 const Layout = () => {
     const contract = useStore((state: Store) => (state.editor ? (state.contractsConfig[state.editor]?._uid ?? null) : null));
+    const { scopeConfig } = useStore();
 
     return (
         <main className='grid grid-rows-[min-content_1fr] grid-cols-[1fr_26rem] h-[100vh] items-stretch justify-stretch max-h-screen gap-4 min-h-0 min-w-0 p-4'>
@@ -28,7 +29,7 @@ const Layout = () => {
                     <ScrollArea className='bg-background border border-border rounded shadow-lg'>
                         <div className='p-6 ppb-0'>
                             <h2 className='text-2xl font-bold mb-4'>Project Editor</h2>
-                            <ScopeEditor />
+                            <ScopeEditor key={scopeConfig.name} />
                         </div>
                     </ScrollArea>
                 </>
