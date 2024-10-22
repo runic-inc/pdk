3421dbce7e121057378863bc015d27f2553765cf
Rob Green <rbgrn@rbgrn.net>
2024-10-22 08:25:00 -0700

    examples in user code

--- packages/common/src/codegen/userContractGen.ts
+++ packages/common/src/codegen/userContractGen.ts
@@ -15,7 +15,22 @@ export class UserContractGen implements Generator {
         `import "./${generatedContractName}.sol";\n` +
         `\n` +
         `contract ${contractName} is ${generatedContractName} {\n` +
-        `  constructor(address _manager, address _owner) ${generatedContractName}(_manager, _owner) {}\n` +
+        `    constructor(address _manager, address _owner) ${generatedContractName}(_manager, _owner) {}\n` +
+        `\n` +
+        `    // Add your custom logic here\n` +
+        `    // This contract will not be overwritten by PDK when regenerating contracts\n` +
+        `    // See https://docs.patchwork.dev for more details\n` +
+        `\n` +
+        `    // example - customizing minting rules for an IPatchworkMintable\n` +
+        `    // function _mintSingle(address to, bytes calldata /* data */) internal override returns (uint256) {\n` +
+        `        // add custom mint rules here\n` +
+        `    // }\n` +
+        `\n` +
+        `    // example - adding a burn function\n` +
+        `    // function burn(uint256 tokenId) internal override {\n` +
+        `        // add custom burn rules here\n` +
+        `        // _burn(tokenId);\n` +
+        `    // }\n` +
         `}\n`;
     }
 }
