a7e11d3118d042fca1de5e651934094efc1a06b8
brockpetrie <brockpetrie@gmail.com>
2024-11-22 15:37:11 -0600

    Update wagmi.config.ts

--- examples/default/wagmi.config.ts
+++ examples/default/wagmi.config.ts
@@ -8,11 +8,10 @@ export default defineConfig([
         out: 'www/src/generated/hooks/wagmi.ts',
         plugins: [
             foundry({
-                include: getContracts(),
+                include: getContracts().map(({ path }) => path),
                 deployments: Object.fromEntries(
-                    getContracts().map((contractName) => {
-                        const contractAddress = getDeploymentAddressForContract(contractName);
-                        return [contractName, contractAddress];
+                    getContracts().map(({ name, address }) => {
+                        return [name, address];
                     }),
                 ),
                 forge: {
@@ -28,23 +27,31 @@ function getDeploymentAddressForContract(contractName: string): `0x${string}` {
     return '0x0000000000000000000000000000000000000000';
 }
 
-function getContracts() {
+type C = {
+    name: string;
+    path: string;
+    address: `0x${string}`;
+};
+function getContracts(): C[] {
     const srcDir = path.resolve(__dirname, './contracts/src');
     const outDir = path.resolve(__dirname, './contracts/out');
     try {
         const solFiles = readdirSync(srcDir);
         const solBaseNames = solFiles.filter((file) => file.endsWith('.sol') && !file.toLowerCase().includes('generated')).map((file) => path.parse(file).name);
 
-        const matchingJsonFiles: string[] = [];
+        const contracts: C[] = [];
         for (const baseName of solBaseNames) {
             const jsonPath = path.join(`${baseName}.sol`, `${baseName}.json`);
             const fullPath = path.join(outDir, jsonPath);
             if (existsSync(fullPath)) {
-                matchingJsonFiles.push(jsonPath);
+                contracts.push({
+                    name: baseName,
+                    path: jsonPath,
+                    address: '0x0000000000000000000000000000000000000000',
+                });
             }
         }
-
-        return matchingJsonFiles;
+        return contracts;
     } catch (err) {
         console.error('Error reading directories:', err);
         return [];
