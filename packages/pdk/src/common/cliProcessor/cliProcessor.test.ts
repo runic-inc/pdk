b5056822cc2019a4f87eb337510c51b245b9ad13
donaldinho <donaldinho@users.noreply.github.com>
2024-11-29 13:10:20 +0000

    fix tests

--- packages/pdk/src/common/cliProcessor/cliProcessor.test.ts
+++ packages/pdk/src/common/cliProcessor/cliProcessor.test.ts
@@ -2,8 +2,8 @@ import fs from 'fs';
 import path from 'path';
 import { CLIProcessor } from './cliProcessor';
 
-const CONTRACT_SCHEMA = '../../../../../schemas/patchwork-contract-config.schema.json';
-const PROJECT_SCHEMA = '../../../../../schemas/patchwork-project-config.schema.json';
+const CONTRACT_SCHEMA = path.join(__dirname, '../../../../../schemas/patchwork-contract-config.schema.json');
+const PROJECT_SCHEMA = path.join(__dirname, '../../../../../schemas/patchwork-project-config.schema.json');
 
 const cliProcessor = new CLIProcessor(CONTRACT_SCHEMA, PROJECT_SCHEMA);
 
