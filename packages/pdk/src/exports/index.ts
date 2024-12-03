51261a43afcb8fa124ff8f117d64521708762e25
Kevin Day <kevin.r.day@gmail.comm>
2024-12-03 16:33:59 -0500

    comment

--- packages/pdk/src/exports/index.ts
+++ packages/pdk/src/exports/index.ts
@@ -1,5 +1,4 @@
 import { ContractProcessor } from '../dev/services/contract-processor';
-//export { ContractProcessor } from '../services/contract-processor';
 
 export const processContracts = async (configPath: string, config = {}, shouldDeploy = false) => {
     const processor = new ContractProcessor();
