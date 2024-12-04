501eb680a4f9a6bef07aaeb0a87e150a71ad793d
Kevin Day <kevin.r.day@gmail.comm>
2024-12-04 17:23:23 -0500

    generate all update

--- packages/pdk/src/generate/all.ts
+++ packages/pdk/src/generate/all.ts
@@ -1,5 +1,25 @@
-import { generateServices } from './services';
+import { cliProcessor } from '../common/cliProcessor';
+import { logger } from '../common/helpers/logger';
+import { generateContractDeployScripts, generateContracts, generateServices } from './';
 
 export async function generateAll(configPath: string) {
+    logger.info('Starting full generation process...');
+
+    // First generate the contracts
+    logger.info('Generating contracts...');
+    await generateContracts([configPath], process.cwd());
+
+    // Generate deploy scripts
+    logger.info('Generating deploy scripts...');
+    await generateContractDeployScripts([configPath], undefined, process.cwd());
+
+    // Build the contracts using cliProcessor
+    logger.info('Building contracts...');
+    await cliProcessor.buildContracts(process.cwd());
+
+    // Generate all services
+    logger.info('Generating services...');
     await generateServices(configPath);
+
+    logger.info('All components generated successfully!');
 }
