e2b6e6ec2977a8841aa17d9809261db33dbf257f
brockpetrie <brockpetrie@gmail.com>
2024-12-05 14:31:01 -0600

    Contract placeholders and config

---
+++ examples/demo-composable-pfp/contracts/src/Character.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import "@patchwork/interfaces/IPatchworkProtocol.sol";
+import "@patchwork/interfaces/IPatchworkMintable.sol";
+import "@patchwork/Patchwork721.sol";
+import "@patchwork/PatchworkLiteRef.sol";
+import "@patchwork/PatchworkUtils.sol";
+import "./CharacterGenerated.sol";
+
+contract Character is CharacterGenerated {
+}
