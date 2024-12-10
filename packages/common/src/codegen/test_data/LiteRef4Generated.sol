ee64f18ff1a91f5a475b23f89472fbc64ce40ef6
Rob Green <rbgrn@rbgrn.net>
2024-12-10 09:37:59 -0800

    Make mintable functions virtual

--- packages/common/src/codegen/test_data/LiteRef4Generated.sol
+++ packages/common/src/codegen/test_data/LiteRef4Generated.sol
@@ -75,14 +75,14 @@ abstract contract LiteRef4Generated is Patchwork721, PatchworkLiteRef, IPatchwor
         return data;
     }
 
-    function mint(address to, bytes calldata data) public payable returns (uint256 tokenId) {
+    function mint(address to, bytes calldata data) public virtual payable returns (uint256 tokenId) {
         if (msg.sender != _manager) {
             return IPatchworkProtocol(_manager).mint{value: msg.value}(to, address(this), data);
         }
         return _mintSingle(to, data);
     }
 
-    function mintBatch(address to, bytes calldata data, uint256 quantity) public payable returns (uint256[] memory tokenIds) {
+    function mintBatch(address to, bytes calldata data, uint256 quantity) public virtual payable returns (uint256[] memory tokenIds) {
         if (msg.sender != _manager) {
             return IPatchworkProtocol(_manager).mintBatch{value: msg.value}(to, address(this), data, quantity);
         }
@@ -92,7 +92,7 @@ abstract contract LiteRef4Generated is Patchwork721, PatchworkLiteRef, IPatchwor
         }
     }
 
-    function _mintSingle(address to, bytes calldata /* data */) internal returns (uint256) {
+    function _mintSingle(address to, bytes calldata /* data */) internal virtual returns (uint256) {
         uint256 tokenId = _nextTokenId;
         _metadataStorage[tokenId] = new uint256[](1);
         _nextTokenId++;
