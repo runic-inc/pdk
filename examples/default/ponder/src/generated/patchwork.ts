9e2618ef160226a7bc77a76cd204b60963b4b91b
brockpetrie <brockpetrie@gmail.com>
2024-12-10 10:25:27 -0600

    Pass ponder types to evt bus

--- examples/default/ponder/src/generated/patchwork.ts
+++ examples/default/ponder/src/generated/patchwork.ts
@@ -1,23 +1,26 @@
+import { EventNames, IndexingFunctionArgs } from '@/generated';
 import { Effect } from 'effect';
 import { UnknownException } from 'effect/Cause';
 
 export type PatchworkEventBus = {
-    emit: (event: string, payload: unknown) => Promise<void>;
-    after: (event: string, handler: (payload: unknown) => Promise<void>) => void;
+    emit: <T extends EventNames>(event: T, payload: IndexingFunctionArgs<T>) => Promise<void>;
+    after: <T extends EventNames>(event: T, handler: (payload: IndexingFunctionArgs<T>) => Promise<void>) => void;
 };
 
 export const initPatchworkEventBus = (): PatchworkEventBus => {
-    const handlers: Record<string, Array<(payload: unknown) => Effect.Effect<void, UnknownException>>> = {};
+    const handlers: {
+        [T in EventNames]?: Array<(payload: IndexingFunctionArgs<T>) => Effect.Effect<void, UnknownException>>;
+    } = {};
 
     return {
-        emit: async (event, payload) => {
+        emit: async <T extends EventNames>(event: T, payload: IndexingFunctionArgs<T>) => {
             const eventHandlers = handlers[event] || [];
             await Promise.all(eventHandlers.map((handler) => Effect.runPromise(handler(payload))));
         },
 
-        after: (event, handler) => {
+        after: <T extends EventNames>(event: T, handler: (payload: IndexingFunctionArgs<T>) => Promise<void>) => {
             if (!handlers[event]) handlers[event] = [];
-            const wrappedHandler = (payload: unknown) => Effect.tryPromise(() => handler(payload));
+            const wrappedHandler = (payload: IndexingFunctionArgs<T>) => Effect.tryPromise(() => handler(payload));
             handlers[event].push(wrappedHandler);
         },
     };
