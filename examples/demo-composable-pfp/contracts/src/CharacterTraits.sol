6f4a7290f1d198aadb24cc5fa832783063f9c671
brockpetrie <brockpetrie@gmail.com>
2024-12-12 16:22:21 -0600

    Update CharacterTraits.sol

--- examples/demo-composable-pfp/contracts/src/CharacterTraits.sol
+++ examples/demo-composable-pfp/contracts/src/CharacterTraits.sol
@@ -4,8 +4,12 @@ pragma solidity ^0.8.23;
 import "./CharacterTraitsGenerated.sol";
 
 contract CharacterTraits is CharacterTraitsGenerated {
-    // Simplified events - removed isPromo 
-    event Register(uint16 indexed traitId, uint8 indexed traitType, string name);
+    // Simplified events - removed isPromo
+    event Register(
+        uint16 indexed traitId,
+        uint8 indexed traitType,
+        string name
+    );
     event SupplyLock(uint16 indexed traitId);
 
     // Mapping to store trait definitions
@@ -13,10 +17,11 @@ contract CharacterTraits is CharacterTraitsGenerated {
     mapping(uint8 => uint16[]) private _traitsByType;
     // Storage for locked traits
     mapping(uint16 => bool) private _supplyLockedTraits;
-    
-    constructor(address manager_, address owner_) 
-        CharacterTraitsGenerated(manager_, owner_) 
-    {}
+
+    constructor(
+        address manager_,
+        address owner_
+    ) CharacterTraitsGenerated(manager_, owner_) {}
 
     /**
      * @notice Register new traits that can be minted
@@ -33,18 +38,18 @@ contract CharacterTraits is CharacterTraitsGenerated {
             ids.length == names.length && ids.length == traitTypes.length,
             "Arrays must be same length"
         );
-        
+
         for (uint256 i = 0; i < ids.length; i++) {
             require(!_registeredTraits[ids[i]], "Trait already registered");
             require(ids[i] != 0, "Cannot register ID 0");
-            
+
             _registeredTraits[ids[i]] = true;
             _traitsByType[traitTypes[i]].push(ids[i]);
-            
+
             // Pre-register the metadata
             Metadata memory metadata = Metadata({
-                name: names[i],
-                id: ids[i],
+                trait_name: names[i],
+                trait_id: ids[i],
                 trait_type: traitTypes[i]
             });
             _traitDefinitions[ids[i]] = metadata;
@@ -60,22 +65,25 @@ contract CharacterTraits is CharacterTraitsGenerated {
     /**
      * @dev Override _mintSingle to assign trait metadata during mint
      */
-    function _mintSingle(address to, bytes calldata /* data */) internal override returns (uint256) {
+    function _mintSingle(
+        address to,
+        bytes calldata /* data */
+    ) internal override returns (uint256) {
         uint256 tokenId = _nextTokenId;
         _nextTokenId++;
-        
+
         // Get a random registered trait
         uint16 traitId = _getRandomRegisteredTrait();
-        
+
         // Get the predefined metadata for this trait
         Metadata memory metadata = _traitDefinitions[traitId];
-        
+
         // Mint the token
         _safeMint(to, tokenId);
-        
+
         // Store the metadata
         _metadataStorage[tokenId] = packMetadata(metadata);
-        
+
         return tokenId;
     }
 
@@ -85,11 +93,11 @@ contract CharacterTraits is CharacterTraitsGenerated {
     function supplyLock(uint16 traitId) public onlyOwner {
         require(_registeredTraits[traitId], "Trait does not exist");
         require(!_supplyLockedTraits[traitId], "Trait already locked");
-        
+
         uint8 traitType = _traitDefinitions[traitId].trait_type;
         _removeFromTraitType(traitType, traitId);
         _supplyLockedTraits[traitId] = true;
-        
+
         emit SupplyLock(traitId);
     }
 
@@ -116,12 +124,17 @@ contract CharacterTraits is CharacterTraitsGenerated {
     function _getRandomRegisteredTrait() internal view returns (uint16) {
         // Get a random trait type first
         uint8 numTypes = 5; // Assuming we have 5 trait types (0-4)
-        uint8 randomType = uint8(uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender))) % numTypes);
-        
+        uint8 randomType = uint8(
+            uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender))) %
+                numTypes
+        );
+
         uint16[] storage traitsOfType = _traitsByType[randomType];
         require(traitsOfType.length > 0, "No traits registered for type");
-        
-        uint256 randomIndex = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, randomType))) % traitsOfType.length;
+
+        uint256 randomIndex = uint256(
+            keccak256(abi.encodePacked(block.timestamp, msg.sender, randomType))
+        ) % traitsOfType.length;
         return traitsOfType[randomIndex];
     }
 
@@ -135,7 +148,9 @@ contract CharacterTraits is CharacterTraitsGenerated {
     /**
      * @notice Get all trait IDs for a given trait type
      */
-    function getTraitsByType(uint8 traitType) public view returns (uint16[] memory) {
+    function getTraitsByType(
+        uint8 traitType
+    ) public view returns (uint16[] memory) {
         return _traitsByType[traitType];
     }
 }
