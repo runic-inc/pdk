33a4118e78b6dc96889badec5b8102412679131c
brockpetrie <brockpetrie@gmail.com>
2024-12-12 14:56:40 -0600

    Update traits.ts

--- examples/demo-composable-pfp/assets/traits.ts
+++ examples/demo-composable-pfp/assets/traits.ts
@@ -1,9 +1,9 @@
 export enum TraitType {
-    BACKGROUND = 0,
-    CHARACTER = 1,
-    EYES = 2,
-    MOUTH = 3,
-    ACCESSORY = 4
+    BODY = 0,
+    CLOTHING = 1,
+    EXPRESSION = 2,
+    HAIR = 3,
+    ACCESSORY = 4,
 }
 
 export interface Trait {
@@ -15,7 +15,7 @@ export interface Trait {
 
 // Our main contract address
 const TraitContract = {
-    default: process.env.TRAIT_CONTRACT_ADDRESS as `0x${string}`
+    default: process.env.TRAIT_CONTRACT_ADDRESS as `0x${string}`,
 };
 
 type TraitContractKeys = keyof typeof TraitContract;
@@ -31,40 +31,45 @@ for (const key in TraitContract) {
 export const contractLabels: {
     [key in TraitContractKeys]: string;
 } = {
-    default: 'Base Collection'
+    default: 'Base Collection',
 };
 
 // Define traits
 const traitDefinitions: Record<number, Trait> = {
     // Backgrounds
-    1: { id: 1, type: TraitType.BACKGROUND, name: 'Sunset Sky', image: 'backgrounds/sunset.svg' },
-    2: { id: 2, type: TraitType.BACKGROUND, name: 'Starry Night', image: 'backgrounds/starry.svg' },
-    3: { id: 3, type: TraitType.BACKGROUND, name: 'Ocean Waves', image: 'backgrounds/ocean.svg' },
-    
+    1: { id: 1, type: TraitType.BODY, name: 'Pale', image: 'body/pale.svg' },
+    2: { id: 2, type: TraitType.BODY, name: 'Dark', image: 'body/dark.svg' },
+    3: { id: 3, type: TraitType.BODY, name: 'Yellow', image: 'body/yellow.svg' },
+    4: { id: 4, type: TraitType.BODY, name: 'Green', image: 'body/green.svg' },
+
     // Characters
-    11: { id: 11, type: TraitType.CHARACTER, name: 'Robot', image: 'characters/robot.svg' },
-    12: { id: 12, type: TraitType.CHARACTER, name: 'Alien', image: 'characters/alien.svg' },
-    13: { id: 13, type: TraitType.CHARACTER, name: 'Ninja', image: 'characters/ninja.svg' },
-    
+    11: { id: 11, type: TraitType.CLOTHING, name: 'Skull Tee', image: 'clothing/skulltee.svg' },
+    12: { id: 12, type: TraitType.CLOTHING, name: 'Hoodie', image: 'clothing/hoodie.svg' },
+    13: { id: 13, type: TraitType.CLOTHING, name: 'Blazer', image: 'clothing/blazer.svg' },
+    14: { id: 14, type: TraitType.CLOTHING, name: 'Overalls', image: 'clothing/overalls.svg' },
+
     // Eyes
-    21: { id: 21, type: TraitType.EYES, name: 'Sleepy', image: 'eyes/sleepy.svg' },
-    22: { id: 22, type: TraitType.EYES, name: 'Curious', image: 'eyes/curious.svg' },
-    23: { id: 23, type: TraitType.EYES, name: 'Excited', image: 'eyes/excited.svg' },
-    
+    21: { id: 21, type: TraitType.EXPRESSION, name: 'Angry', image: 'expression/angry.svg' },
+    22: { id: 22, type: TraitType.EXPRESSION, name: 'Happy', image: 'expression/happy.svg' },
+    23: { id: 23, type: TraitType.EXPRESSION, name: 'Sad', image: 'expression/sad.svg' },
+    24: { id: 24, type: TraitType.EXPRESSION, name: 'Silly', image: 'expression/silly.svg' },
+
     // Mouths
-    31: { id: 31, type: TraitType.MOUTH, name: 'Happy', image: 'mouths/happy.svg' },
-    32: { id: 32, type: TraitType.MOUTH, name: 'Surprised', image: 'mouths/surprised.svg' },
-    33: { id: 33, type: TraitType.MOUTH, name: 'Smirk', image: 'mouths/smirk.svg' },
-    
+    31: { id: 31, type: TraitType.HAIR, name: 'Short', image: 'hair/short.svg' },
+    32: { id: 32, type: TraitType.HAIR, name: 'Waves', image: 'hair/waves.svg' },
+    33: { id: 33, type: TraitType.HAIR, name: 'Mullet', image: 'hair/mullet.svg' },
+    34: { id: 34, type: TraitType.HAIR, name: 'Floral', image: 'hair/floral.svg' },
+
     // Accessories
-    41: { id: 41, type: TraitType.ACCESSORY, name: 'Bowtie', image: 'accessories/bowtie.svg' },
-    42: { id: 42, type: TraitType.ACCESSORY, name: 'Necklace', image: 'accessories/necklace.svg' },
-    43: { id: 43, type: TraitType.ACCESSORY, name: 'Watch', image: 'accessories/watch.svg' }
+    41: { id: 41, type: TraitType.ACCESSORY, name: 'Glasses', image: 'accessories/glasses.svg' },
+    42: { id: 42, type: TraitType.ACCESSORY, name: 'Sunglasses', image: 'accessories/sunglasses.svg' },
+    43: { id: 43, type: TraitType.ACCESSORY, name: 'Winter Hat', image: 'accessories/winterhat.svg' },
+    44: { id: 44, type: TraitType.ACCESSORY, name: 'Hat', image: 'accessories/hat.svg' },
 };
 
 // Helper function to get traits by type
 export function getTraitsByType(type: TraitType): Trait[] {
-    return Object.values(traitDefinitions).filter(trait => trait.type === type);
+    return Object.values(traitDefinitions).filter((trait) => trait.type === type);
 }
 
 export { TraitContract, traitDefinitions as traits };
