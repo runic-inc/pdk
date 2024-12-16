68ea3d3612110b89bdf22516e2073926bba486b9
brockpetrie <brockpetrie@gmail.com>
2024-12-16 15:04:49 -0600

    Icons assets etc

--- examples/demo-composable-pfp/ponder/src/index.ts
+++ examples/demo-composable-pfp/ponder/src/index.ts
@@ -1,4 +1,34 @@
-import { ponder } from '@/generated';
+import { characterTraits } from '../ponder.schema';
+import { patchwork } from './generated/patchwork';
 
-// Need logic here that extends our built-in event handlers and calls asset service for asset gen
-const deleteme = typeof ponder;
+patchwork.after('CharacterTraits:Transfer', async ({ event, context }) => {
+	const [trait_id, trait_name, trait_type] = await Promise.all([
+		context.client.readContract({
+			abi: context.contracts.CharacterTraits.abi,
+			address: context.contracts.CharacterTraits.address,
+			functionName: 'loadTraitid',
+			args: [event.args.tokenId],
+		}),
+		context.client.readContract({
+			abi: context.contracts.CharacterTraits.abi,
+			address: context.contracts.CharacterTraits.address,
+			functionName: 'loadTraitname',
+			args: [event.args.tokenId],
+		}),
+		context.client.readContract({
+			abi: context.contracts.CharacterTraits.abi,
+			address: context.contracts.CharacterTraits.address,
+			functionName: 'loadTraittype',
+			args: [event.args.tokenId],
+		}),
+	]);
+	await context.db
+		.update(characterTraits, {
+			id: `${event.log.address}:${event.args.tokenId}`,
+		})
+		.set({
+			trait_id,
+			trait_name,
+			trait_type,
+		});
+});
